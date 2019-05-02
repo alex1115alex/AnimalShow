33854edbb4571710fec856078a5b9c03ec9d45ff
Alex Israelov <alex1115alex@gmail.com>
2019-05-01 18:48:49 -0600

    sad

---
+++ COMPILE.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#Compiles AnimalShow
+g++ -std=c++11 -w -o AnimalShow.out Hmwk9.cpp
---
+++ Run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#AnimalShow.out
+#g++ -std=c++11 -w -o AnimalShow.out Hmwk9.cpp
+./AnimalShow.out
